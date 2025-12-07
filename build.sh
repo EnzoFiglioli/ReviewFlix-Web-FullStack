6bad137a6535180e633244badf6b29ec00188b98
Enzo Figlioli <105600952+EnzoFiglioli@users.noreply.github.com>
2025-12-07 16:00:43 -0300

    Create build.sh for environment setup and migration
    Add build script for setting up the environment and running migrations.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+```
+
+### **3. En Render, cambiá el Start Command**
+
+En la configuración de Render, donde dice **"Start Command"**, poné:
+```
+gunicorn reviewflix.wsgi:application
