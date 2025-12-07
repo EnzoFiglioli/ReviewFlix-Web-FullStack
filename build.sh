b56f21f0f74b2c0aea8c431e0f4c073bf85e17ef
Enzo Figlioli <105600952+EnzoFiglioli@users.noreply.github.com>
2025-12-07 16:40:41 -0300

    Clean up build.sh by removing comments and shebang
    Removed shebang and unnecessary comments from build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-### **3. En Render, cambiá el Start Command**
-
-En la configuración de Render, donde dice **"Start Command"**, poné:
-```
-gunicorn Reviewflix.wsgi:application
